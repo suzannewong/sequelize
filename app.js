a74b642d4a7958e8fc18c27639c7455b6ead24ab
Duchess <veraruthkahn@gmail.com>
2021-08-12 12:58:46 -0400

    created models

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const fs = require('fs')
+const layout = require('./views/layout')
+const { db } = require('./models')
 
 const app = express();
+
 app.use(morgan("dev"));
-app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname), "public"));
 //Extended False = cannot post nested object
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, "public")));
 
 
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 //Default Route
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  res.send(layout(''));
 })
 
 const PORT = 1337;
---
+++ models/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+module.exports = {
+  db
+}
+
+const Page = db.define('page',{
+    title : Sequelize.STRING,
+    slug : Sequelize.TEXT,
+    content : Sequelize.TEXT,
+    status : Sequelize.BOOLEAN
+})
+
+const User = db.define('user',{
+    name : Sequelize.STRING,
+    email : Sequelize.STRING
+})
